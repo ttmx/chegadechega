01d0b8974eb8e313a059176aea24ed21ef1213b4
Tiago Teles <tiago.sequeira.teles@gmail.com>
2020-08-14 00:23:09 +0100

    Added scrollbar display thingy

--- script.js
+++ script.js
@@ -5,3 +5,35 @@ function toggleCol(theme){
 
 	})
 }
+
+let types = ["fasc","lgbt","edu","fam"];
+
+function makeSpanList(){
+	let hilights = {};
+	let final_hilight = {};
+	for (i of types){
+		hilights[i] = document.getElementsByClassName(i);
+		final_hilight[i] = [];
+		for(a of hilights[i])
+			if (a.localName == "span")
+				final_hilight[i].push(a);
+	}
+	return final_hilight;
+}
+
+
+function makeBoxes(){
+	let scroller = document.getElementById("scrollbar");
+	let scale = window.innerHeight / document.body.getBoundingClientRect().height;
+	for(type of Object.values(makeSpanList()))
+		for (elem of type){
+			let bar = document.createElement("div");
+			bar.className = elem.className;
+			bar.style.height = elem.getBoundingClientRect().height * scale + "px";
+			bar.style.top = (elem.getBoundingClientRect().y + document.documentElement.scrollTop) * scale + "px";
+			bar.style.position = "relative";
+			scroller.appendChild(bar);
+		}
+}
+
+makeBoxes();
