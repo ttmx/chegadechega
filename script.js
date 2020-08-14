9b65178732954b3c0d04e6f832b282db6e514697
Tiago Teles <tiago.sequeira.teles@gmail.com>
2020-08-15 00:43:04 +0100

    Added anti spam

--- script.js
+++ script.js
@@ -66,11 +66,15 @@ function jumpTo(type) {
 
 }
 
+let toastLock = false;
 function toast(msg){
-	var x = document.getElementById("snackbar");
-	x.innerHTML = msg;
-	x.className = "show";
-	setTimeout(function () {x.className = x.className.replace("show", "");}, 3000);
+	if (!toastLock){
+		toastLock = true;
+		var x = document.getElementById("snackbar");
+		x.innerHTML = msg;
+		x.className = "show";
+		setTimeout(function () {x.className = x.className.replace("show", ""); toastLock = false;}, 3000);
+	}
 }
 
 
