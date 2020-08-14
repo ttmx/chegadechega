74fd58149c5ecaee1a6a04f774b8fc57083dba94
Tiago Teles <tiago.sequeira.teles@gmail.com>
2020-08-15 00:30:54 +0100

    Mostly done with click to scroll

--- script.js
+++ script.js
@@ -7,16 +7,27 @@ function toggleCol(theme){
 }
 
 let types = ["fasc","lgbt","edu","fam"];
+let labels = {
+	'fasc':'Autoritarismo',
+	'lgbt':'LGBT+',
+	'edu':'Educação',
+	'fam':'Família'
+}
 
+function pruneByClass(elems,className){
+	let toReturn = []
+		for(a of elems)
+			if (a.localName == className)
+				toReturn.push(a);
+	return toReturn;
+}
 function makeSpanList(){
 	let hilights = {};
 	let final_hilight = {};
 	for (i of types){
 		hilights[i] = document.getElementsByClassName(i);
 		final_hilight[i] = [];
-		for(a of hilights[i])
-			if (a.localName == "span")
-				final_hilight[i].push(a);
+		final_hilight[i] = pruneByClass(hilights[i],"span");
 	}
 	return final_hilight;
 }
@@ -36,9 +47,30 @@ function makeBoxes(){
 			bar.onclick = (function(currElem) {
 				return function() { currElem.scrollIntoView(); };
 			 })(elem);
-			console.log(elem);
 			scroller.appendChild(bar);
 		}
+}
+
+function jumpTo(type) {
+	let spans = pruneByClass(document.getElementsByClassName(type),"span");
+	let found = false;
+	for (span of spans){
+		if(span.getBoundingClientRect().y>20){
+			span.scrollIntoView();
+			found = true;
+			break;
+		}
+	}
+	if (!found)
+		toast("Fim de medidas sobre "+labels[type]);
+
+}
+
+function toast(msg){
+	var x = document.getElementById("snackbar");
+	x.innerHTML = msg;
+	x.className = "show";
+	setTimeout(function () {x.className = x.className.replace("show", "");}, 3000);
 }
 
 
@@ -46,5 +78,5 @@ document.addEventListener("DOMContentLoaded", function() {
     //The first argument are the elements to which the plugin shall be initialized
     //The second argument has to be at least a empty object or a object with your desired options
 	makeBoxes();
-    OverlayScrollbars(document.querySelectorAll('body'), { });
+    // OverlayScrollbars(document.querySelectorAll('body'), { });
 });
