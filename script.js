8c0be9092e0f59ca19c8c94e0e5776ac7adc0c38
Tiago Teles <tiago.sequeira.teles@gmail.com>
2020-08-15 20:24:30 +0100

    Purposefully broken highlights

--- script.js
+++ script.js
@@ -81,10 +81,22 @@ function toast(msg){
 	}
 }
 
+function addQuoteListeners(){
+	let spans = document.getElementById("programa").getElementsByTagName("span");
+	for(span of spans){
+		span.addEventListener("click",function(a,b,c){
+			console.log(a);
+			console.log(this);
+			console.log("---");
+		})
+	}
+}
+
 
 document.addEventListener("DOMContentLoaded", function() {
     //The first argument are the elements to which the plugin shall be initialized
     //The second argument has to be at least a empty object or a object with your desired options
 	makeBoxes();
     OverlayScrollbars(document.querySelectorAll('body'), { });
+	addQuoteListeners();
 });
