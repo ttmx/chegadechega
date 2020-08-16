9609c0cc632c37ddd3840397d7dd447fabe24048
Tiago Teles <tiago.sequeira.teles@gmail.com>
2020-08-16 02:12:08 +0100

    Just testing

--- script.js
+++ script.js
@@ -88,7 +88,7 @@ function addQuoteListeners(){
 document.addEventListener("DOMContentLoaded", function() {
     //The first argument are the elements to which the plugin shall be initialized
     //The second argument has to be at least a empty object or a object with your desired options
-	makeBoxes();
+	// makeBoxes();
     OverlayScrollbars(document.querySelectorAll('body'), { });
-	addQuoteListeners();
+	// addQuoteListeners();
 });
